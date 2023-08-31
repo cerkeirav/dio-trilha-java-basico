72873197df27e782f7bfb576bc5ecdd3c18d99d1
Gabriel Cerqueira <gabrielcerqueirav@gmail.com>
2023-08-30 23:36:28 -0300

    enviando desafio de projeto controle de fluxo/contador

--- DesafioControleFluxo/src/desafioContador/Contador.java
+++ DesafioControleFluxo/src/desafioContador/Contador.java
@@ -1,10 +1,45 @@
 package desafioContador;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Contador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		int primeiroNumero;
+		int segundoNumero;
+		
+		while (true) {
+			System.out.println("INSIRA O PRIMEIRO NUMERO: ");
+			try {
+				primeiroNumero = sc.nextInt();
+				System.out.println("INSIRA O SEGUNDO NUMERO: ");
+				segundoNumero = sc.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+				sc.nextLine();
+				System.out.println("INSERIR APENAS NUMEROS");
+			}
+		}
+		
+		try {
+			validarNumeros(primeiroNumero, segundoNumero);
+		} catch (ParametrosInvalidosException e) {
+			System.out.println("O PRIMEIRO VALOR DEVE SER MENOR QUE O SEGUNDO.");
+		}
+		sc.close();
+			
+	}
+	static void validarNumeros(int numeroUm, int numeroDois) throws ParametrosInvalidosException {
+		if(numeroUm > numeroDois) {
+			throw new ParametrosInvalidosException();
+		}else {
+			int terceiroNumero = numeroDois - numeroUm;
+			for(int i = 1; i <= terceiroNumero; i++) {
+				System.out.println("Imprimindo o numero " + i);
+			}
+		}
 	}
 
 }
---
+++ DesafioControleFluxo/src/desafioContador/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+package desafioContador;
+
+public class ParametrosInvalidosException extends Exception{
+	
+	
+	 public ParametrosInvalidosException() {
+	        super("Parâmetros inválidos.");
+	    }
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
