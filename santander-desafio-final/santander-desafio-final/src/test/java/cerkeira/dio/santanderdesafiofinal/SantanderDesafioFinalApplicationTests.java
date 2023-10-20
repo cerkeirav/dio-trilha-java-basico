6ee505b3253a08078f2cf364b3129ab4f16b9979
Gabriel Cerqueira <gabrielcerqueirav@gmail.com>
2023-10-20 20:42:26 -0300

    Commit inicial desafio final bootcamp

---
+++ santander-desafio-final/santander-desafio-final/src/test/java/cerkeira/dio/santanderdesafiofinal/SantanderDesafioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package cerkeira.dio.santanderdesafiofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDesafioFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
